bd2d87aa1611f37705e243bc91ba8fb4e3c8573c
Pablo Gimenez <pegimenez@fi.uba.ar>
2024-04-02 18:33:16 -0300

    Practica 5 Ejercicio 2 - Se incluyo la API UART al ejercicio Practica 4 Ejercicio 2

---
+++ Practica5_Punto2/Drivers/API/Inc/API_uart.h
@@ -0,0 +1,52 @@
+ /**
+  ******************************************************************************
+  * @file    API_uart.h
+  * @author  Pablo Gimenez
+  * @brief   Archivo que contiene las declaraciones de comunicacion UART
+  ******************************************************************************
+ */
+
+#ifndef API_INC_API_UART_H_
+#define API_INC_API_UART_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdbool.h>
+#include "stm32f4xx_hal.h"  		/* <- HAL include */
+#include "stm32f4xx_nucleo_144.h" 	/* <- BSP include */
+
+/* Defines -------------------------------------------------------------------*/
+#define UART_INIT_OK true
+#define UART_INIT_NOK false
+
+//TODO: Revisar si esto esta correcto como se implementa a modificarlo.
+
+/* User can use this section to tailor USARTx/UARTx instance used and associated
+   resources */
+/* Definition for USARTx clock resources */
+#define USARTx                           USART3
+#define USARTx_CLK_ENABLE()              __HAL_RCC_USART3_CLK_ENABLE();
+#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+
+#define USARTx_FORCE_RESET()             __HAL_RCC_USART3_FORCE_RESET()
+#define USARTx_RELEASE_RESET()           __HAL_RCC_USART3_RELEASE_RESET()
+
+/* Definition for USARTx Pins */
+#define USARTx_TX_PIN                    GPIO_PIN_8
+#define USARTx_TX_GPIO_PORT              GPIOD
+#define USARTx_TX_AF                     GPIO_AF7_USART3
+#define USARTx_RX_PIN                    GPIO_PIN_9
+#define USARTx_RX_GPIO_PORT              GPIOD
+#define USARTx_RX_AF                     GPIO_AF7_USART3
+
+/*Definicion de tipos*/
+typedef bool bool_t;
+
+/*Definicion de funciones*/
+bool_t uartInit();
+void uartSendString(uint8_t * pstring);
+void uartSendStringSize(uint8_t * pstring, uint16_t size);
+void uartReceiveStringSize(uint8_t * pstring, uint16_t size);
+
+#endif /* API_INC_API_UART_H_ */
---
+++ Practica5_Punto2/Drivers/API/Src/API_debounce.c
@@ -0,0 +1,172 @@
+ /**
+  ******************************************************************************
+  * @file    API_debounce.c
+  * @author  Pablo Gimenez
+  * @brief   Archivo de la implementacion de las funciones debounde de MEF
+  ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "API_debounce.h"
+#include "API_uart.h"
+
+/* Defines  ------------------------------------------------------------------*/
+#define INFO_TEXT_FLANCO_DESCENDENTE "Se produjo flanco descendente\n"
+#define INFO_TEXT_FLANCO_ASCENDENTE "Se produjo flanco ascendente\n"
+
+/* Private typedef -----------------------------------------------------------*/
+typedef enum{
+	BUTTON_UP,
+	BUTTON_FALLING,
+	BUTTON_DOWN,
+	BUTTON_RAISING,
+} debounceState_t;
+
+/* Private Variables Declaration ----------------------------------------------*/
+static debounceState_t estadoMEF;
+
+static bool_t buttonPress = false;
+
+static delay_t delayANTIBOUNCE, *p_delayANTIBOUNCE;
+
+
+/* Private Functions Declaration ---------------------------------------------*/
+static void buttonPressed(void);
+static void buttonReleased(void);
+static void API_DEBOUNCE_Error_Handler(void);
+
+/* Desarrollo de las funciones solicitadas */
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Funcion que inicializa la Maquina de Estados Finitos con un estado definido
+  * 		por default y la inicializacion de la UART
+  * @param  None
+  * @retval None
+  */
+void debounceFSM_init(void){
+
+	estadoMEF = BUTTON_UP;
+
+	p_delayANTIBOUNCE = &delayANTIBOUNCE;
+
+	delayInit( p_delayANTIBOUNCE , DELAY_BOUNCE);
+
+	/* Iniciacion del uart */
+	uartInit();
+
+}
+
+/**
+  * @brief  Funcion que actualiza el estado de la Maquina de Estados Finitos según haya cambios en la
+  * 		entrada (pulsador presionado) o en base al tiempo transcurrido desde el cambio de estado
+  * 		de la entrada en las anteriores ejecuciones
+  * @param  None
+  * @retval None
+  */
+void debounceFSM_update(void){
+
+	switch(estadoMEF){
+
+	case BUTTON_UP:
+		if(BSP_PB_GetState(BUTTON_USER)){
+			delayRead (p_delayANTIBOUNCE);
+			estadoMEF = BUTTON_FALLING;
+		}
+		break;
+
+	case BUTTON_FALLING:
+		if(delayRead(p_delayANTIBOUNCE)){
+			if(BSP_PB_GetState(BUTTON_USER)){
+				estadoMEF = BUTTON_DOWN;
+				buttonPressed();
+			}else
+				estadoMEF = BUTTON_UP;
+		}
+		break;
+
+	case BUTTON_DOWN:
+		if(!BSP_PB_GetState(BUTTON_USER)){
+			delayRead (p_delayANTIBOUNCE);
+			estadoMEF = BUTTON_RAISING;
+		}
+		break;
+
+	case BUTTON_RAISING:
+		if(delayRead (p_delayANTIBOUNCE)){
+			if(!BSP_PB_GetState(BUTTON_USER)){
+				estadoMEF = BUTTON_UP;
+				buttonReleased();
+			}else
+				estadoMEF = BUTTON_DOWN;
+		}
+		break;
+
+	/*CASO DEFAULT: En caso de no haber ejecutado una accion por los posibles
+	 * estados anteriornes indica que la maquina de estado no se encuentra en
+	 * un estado posible o válido por algun motivo por lo cual se llama a la
+	 * funcion error
+	 */
+	default:
+		API_DEBOUNCE_Error_Handler();
+		break;
+	}
+
+}
+/**
+  * @brief	Funcion que indica si el pulsador fue presionado. Se coloca en TRUE (presionado) solamente cuando se solto el pulsador
+  * 		es decir con un flanco descendente. Una vez que entrega TRUE por haber presionado limpia la variable privada de estado
+  * @param  None
+  * @retval	TRUE en caso que ha existido un pulso descendente del pulsador - FALSE cuando no se ha presionado o el pulsador sigue en estado presionado
+  */
+bool_t readKey(void){
+
+	bool_t value = false;
+
+	if(buttonPress){
+		value = true;
+		buttonPress = false;
+	}
+
+	return value;
+}
+
+/**
+  * @brief  Funcion que ejecuta accion si se presiona el boton de usuario. Se envia mensaje por UART
+  * @param  None
+  * @retval None
+  */
+void buttonPressed(void){
+
+	uint8_t textToSend[] = INFO_TEXT_FLANCO_DESCENDENTE;
+
+	uartSendString(textToSend);
+
+
+}
+
+/**
+  * @brief  Funcion que realiza accion de cambiar variable global privada de boton presionado. Se envia mensaje por UART
+  * @param  None
+  * @retval None
+  */
+
+void buttonReleased(void){
+
+	buttonPress = true;
+
+	uint8_t textToSend[] = INFO_TEXT_FLANCO_ASCENDENTE;
+
+	uartSendString(textToSend);
+}
+
+static void API_DEBOUNCE_Error_Handler(void)
+{
+	/* Turn LED2 on */
+	BSP_LED_On(LED1);
+	while (1)
+	{
+	}
+}
+
